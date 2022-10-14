5e69fb4692405b07f51036d98655e843c27fc3ca
Adam Corbin <acorbin3@gmail.com>
2022-10-14 04:05:34 -0400

    Updating the splitting_the_data.py code to encode the labels to numbers

--- splitting_the_data.py
+++ splitting_the_data.py
@@ -1,10 +1,15 @@
 from fast_ml.model_development import train_valid_test_split
 import pandas as pd
+from sklearn import preprocessing
+label_encoder = preprocessing.LabelEncoder()
 
 df = pd.read_csv('fitzpatrick17k.csv', low_memory=False)
 
+# The data needs to be encoded because the model is expecting a number
+df['label'] = label_encoder.fit_transform(df['label'])
 
-X_train, y_train, X_valid, y_valid, X_test, y_test = train_valid_test_split(df, target='fitzpatrick_scale',
+
+X_train, y_train, X_valid, y_valid, X_test, y_test = train_valid_test_split(df, target = 'fitzpatrick_scale',
                                                                             train_size=0.8, valid_size=0.1, test_size=0.1,
                                                                             random_state=42)
 
