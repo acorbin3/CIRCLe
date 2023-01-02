3825a7fc6d741599b0aa1b48eecde1c208791955
Adam Corbin <acorbin3@gmail.com>
2023-01-02 05:04:06 -0500

    turning off debuggin

--- models/circle.py
+++ models/circle.py
@@ -17,9 +17,9 @@ class Model(BaseModel):
         super(Model, self).__init__(hidden_dim, base)
 
         self.out_layer = nn.Linear(hidden_dim, config.num_classes)
-        #self.trans = load_stargan(
+        # self.trans = load_stargan(
         #    config.gan_path + 'stargan_last-G.ckpt')
-        #self.trans.eval()
+        # self.trans.eval()
 
         self.alpha = config.alpha
 
@@ -29,26 +29,27 @@ class Model(BaseModel):
         pass
 
     def forward(self, input_image, expected_classification, d=None, input_mask=None):
+        debugging = False
         # run the input into the base model
         z = F.relu(self.base(input_image))
-        print("################")
-        debug_it("orig_z", z)
+        if debugging: print("################")
+        if debugging: debug_it("orig_z", z)
         # run the output of the base into the output layer to determine the class
         logits = self.out_layer(z)
-        debug_it("logits", logits, True)
-        debug_it("y", expected_classification, True)
+        if debugging: debug_it("logits", logits, True)
+        if debugging: debug_it("y", expected_classification, True)
 
         # compute the loss based on the expected y value
         loss = F.cross_entropy(logits, expected_classification)
-        debug_it("loss", loss, True)
+        if debugging: debug_it("loss", loss, True)
 
         # some kind of correction, not quite sure
         correct = (torch.argmax(logits, 1) == expected_classification).sum().float() / input_image.shape[0]
-        debug_it("correct", correct, True)
+        if debugging: debug_it("correct", correct, True)
 
         # empty regurlization
         reg = loss.new_zeros([1])
-        debug_it("reg", reg, True)
+        if debugging: debug_it("reg", reg, True)
         if self.training:
             if self.use_reg:
                 with torch.no_grad():
@@ -60,22 +61,21 @@ class Model(BaseModel):
                     d_new = torch.randint(0, 6, (d.size(0),)).to(d.device)
                     d_new_onehot = d.new_zeros([d.shape[0], 6])
                     d_new_onehot.scatter_(1, d_new[:, None], 1)
-                    #TODO - update to new image transformer
+                    # TODO - update to new image transformer
                     x_new = input_image
                     # New generated image
-                    #x_new = self.trans(x, d_onehot, d_new_onehot)
+                    # x_new = self.trans(x, d_onehot, d_new_onehot)
 
                     # x_new = self.custom_transformer(x)
 
                     # TODO - figure out dimentions
-                    debug_it("x_new", x_new, False)
+                    if debugging: debug_it("x_new", x_new, False)
 
                     # print(x_new)
 
                 z_new = F.relu(self.base(x_new))
-                debug_it("z_new", z_new, False)
 
                 reg = self.alpha * F.mse_loss(z_new, z)
-                debug_it("reg", reg, True)
-        print("------------------")
+                if debugging: debug_it("reg", reg, True)
+        if debugging: print("------------------")
         return loss, reg, correct
