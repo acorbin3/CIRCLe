59251ce8572ff68d883de290733fd47f23296860
Adam Corbin <acorbin3@gmail.com>
2023-01-10 11:00:16 -0500

    Updating CNN model

--- models/cnn.py
+++ models/cnn.py
@@ -5,40 +5,32 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
 
-        # First convolutional layer
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.pool1 = nn.MaxPool2d(2)
+        self.base = nn.sequential(
+            # First convolutional layer
+            nn.Conv2d(3, 16, kernel_size=3, padding=1),
+            nn.ReLu(),
+            nn.MaxPool2d(2),
+            # Second convolutional layer
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.ReLu(),
+            nn.MaxPool2d(2),
+            # Third convolutional layer
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        nn.ReLu(),
+        nn.MaxPool2d(2),
+        # Flatten the feature map
+        nn.Flatten(),
+        nn.Linear(64 * 16 * 16, 512))
 
-        # Second convolutional layer
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.pool2 = nn.MaxPool2d(2)
-
-        # Third convolutional layer
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.pool3 = nn.MaxPool2d(2)
-
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(64 * 16 * 16, 512)
-        self.dropout = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(512, 7)
+        self.output_layer = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(512, 7)
+        )
 
     def forward(self, x):
-        # Pass input through first convolutional layer
-        x = self.pool1(F.relu(self.conv1(x)))
-
-        # Pass output through second convolutional layer
-        x = self.pool2(F.relu(self.conv2(x)))
-
-        # Pass output through third convolutional layer
-        x = self.pool3(F.relu(self.conv3(x)))
-
-        # Flatten the feature map
-        x = self.flatten(x)
-        x = self.fc1(x)
-        base = self.dropout(x)
-        logits = self.fc2(base)
-
-        return logits, base
+        base_output = self.base(x)
+        logist = self.output_layer(base_output)
+        return logist, base_output
 
 
 # Initialize the model
