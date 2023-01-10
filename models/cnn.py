0eb5ac39c6fa623a34cf94147e6c3ddc901d490f
Adam Corbin <acorbin3@gmail.com>
2023-01-10 12:05:31 -0500

    fixing typeo

--- models/cnn.py
+++ models/cnn.py
@@ -8,19 +8,20 @@ class CNN(nn.Module):
         self.base = nn.Sequential(
             # First convolutional layer
             nn.Conv2d(3, 16, kernel_size=3, padding=1),
-            nn.ReLu(),
+            nn.ReLU(),
             nn.MaxPool2d(2),
             # Second convolutional layer
             nn.Conv2d(16, 32, kernel_size=3, padding=1),
-            nn.ReLu(),
+            nn.ReLU(),
             nn.MaxPool2d(2),
             # Third convolutional layer
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
-        nn.ReLu(),
-        nn.MaxPool2d(2),
-        # Flatten the feature map
-        nn.Flatten(),
-        nn.Linear(64 * 16 * 16, 512))
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            # Flatten the feature map
+            nn.Flatten(),
+            nn.Linear(64 * 16 * 16, 512)
+        )
 
         self.output_layer = nn.Sequential(
             nn.Dropout(0.2),
