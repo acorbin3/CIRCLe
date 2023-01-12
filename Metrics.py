a76f39bda7e67ba7e3f37eddd8642f7ac2f6cdad
Adam Corbin <acorbin3@gmail.com>
2023-01-12 02:22:21 -0500

    making the loss function change able also init reg to None

--- Metrics.py
+++ Metrics.py
@@ -6,16 +6,19 @@ from sklearn.metrics import confusion_matrix
 
 
 class Metrics:
-    def __init__(self):
+    def __init__(self, loss_function):
         self.loss_meter = AverageMeter()
         self.accuracy_meter = AverageMeter()
         self.precision_meter = AverageMeter()
         self.recall_meter = AverageMeter()
         self.regularization_meter = AverageMeter()
+        self.loss_function = loss_function
+        self.reg = None
 
     def compute_metrics(self, logits, labels):
         # Compute metrics for main input image
-        self.loss = F.cross_entropy(logits, labels)
+        #self.loss = F.cross_entropy(logits, labels)
+        self.loss = self.loss_function(logits, labels)
         self.predictions = torch.argmax(logits, 1)
         # accuracy = (torch.argmax(logits, 1) == labels).sum().float() / inputs.shape[0]
         self.accuracy = torch.mean(torch.eq(self.predictions, labels).float())
@@ -36,6 +39,6 @@ class Metrics:
         self.accuracy_meter.update(self.accuracy.detach().item(), data)
         self.precision_meter.update(self.precision, data)
         self.recall_meter.update(self.recall, data)
-        if self.reg:
+        if self.reg is not None:
             self.regularization_meter.update(self.reg.detach().item(), data)
         return
