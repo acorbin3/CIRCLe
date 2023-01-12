9bc7583e5051764f9fc8c6214924c381b65fc282
Adam Corbin <acorbin3@gmail.com>
2023-01-12 02:24:14 -0500

    changing Adam optimizer printing model summary passing in hidden layers wrapping x,y,transformed_images into the Variable class fixing some prints that were incorrect

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import argparse
 import os, importlib
 from tqdm import tqdm
 import numpy as np
-from torch import nn
+import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
+from torchsummary import summary
 
 from Metrics import Metrics
 from models.cnn import CNN
@@ -29,7 +31,7 @@ parser.add_argument('--gan_path', type=str, default='saved/stargan/')
 parser.add_argument('--model', type=str, default='circle')
 parser.add_argument('--base', type=str, default='vgg16')
 parser.add_argument('--model_save_dir', type=str, default='saved/model/')
-parser.add_argument('--use_reg_loss', type=bool, default=True)
+parser.add_argument('--use_reg_loss', type=bool, default=False)
 flags = parser.parse_args()
 
 if flags.dataset == 'FitzPatrick17k':
@@ -52,7 +54,6 @@ torch.cuda.manual_seed_all(flags.seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
-
 # Data loader.
 if flags.dataset == "FitzPatrick17k":
     train_loader, val_loader, _ = get_fitz_dataloaders(root=flags.data_dir,
@@ -71,10 +72,11 @@ elif flags.dataset == "isic2018":
 if flags.model != "cnn":
     model = importlib.import_module('models.' + flags.model).Model(flags, flags.hidden_dim, flags.base).to(device)
 else:
-    model = CNN()
+    model = CNN(16)
     model.cuda()
 
-#TODO - consider changing the optimizer. Here are some notes:
+summary(model, (3, 128, 128))
+# TODO - consider changing the optimizer. Here are some notes:
 """
 Stochastic Gradient Descent (SGD): This is a simple and widely-used optimizer that updates the model weights by computing the gradient of the loss function with respect to the weights, and moving the weights in the opposite direction. SGD is sensitive to the learning rate, which controls the step size of the weight updates.
 
@@ -86,7 +88,8 @@ LBFGS (Limited-memory Broyden–Fletcher–Goldfarb–Shanno): This is an optimi
 
 There are many other optimizers available in PyTorch, such as Adagrad, Adadelta, and others
 """
-optim = torch.optim.SGD(model.parameters(), lr=flags.lr, weight_decay=flags.weight_decay, momentum=0.9)
+# optim = torch.optim.SGD(model.parameters(), lr=flags.lr, weight_decay=flags.weight_decay, momentum=0.9)
+optim = torch.optim.Adam(model.parameters(), lr=1e-3)
 
 
 def to_device(data):
@@ -106,18 +109,21 @@ best_val_recall = 0
 
 # TODO- update to use pytorch metrics: https://torchmetrics.readthedocs.io/en/stable/pages/overview.html
 
-
+metrics = Metrics(nn.CrossEntropyLoss().to(device))
+val_metrics = Metrics(nn.CrossEntropyLoss().to(device))
 for epoch in range(flags.epochs):
-    print(f'Epoch {epoch}: Best val loss {best_val_loss}, Best val acc {best_val_acc}, best val recall {best_val_recall}, best val precision {best_val_precision}')
+    print(
+        f'Epoch {epoch}: Best val loss {best_val_loss:.4f}, Best val acc {best_val_acc:.4f}, best val recall {best_val_recall:.4f}, best val precision {best_val_precision:.4f}')
     model.train()
-    metrics = Metrics()
-    for data in tqdm(train_loader, ncols=75, leave=False):
-        data = to_device(data)
-        #optim.zero_grad()
-        #insead of optim.zero_grad(), the below lines are suppose to be faster
+
+    for x, y, transformed_image in tqdm(train_loader, ncols=75, leave=False):
+
+        # optim.zero_grad()
+        # insead of optim.zero_grad(), the below lines are suppose to be faster
         for param in model.parameters():
             param.grad = None
-        x, y, transformed_image = data[0] , data[1], data[2]
+
+        x, y, transformed_image = Variable(x).to(device), Variable(y).to(device), Variable(transformed_image).to(device)
         inputs, labels = x, y
         inputs_transformed, labels_transformed = transformed_image, y
 
@@ -127,23 +133,22 @@ for epoch in range(flags.epochs):
         # Compute metrics for main input image
         metrics.compute_metrics(logits, labels)
 
-        if flags.use_reg_loss:
+        if False:
             logits_transformed, base_output_transformed = model(inputs_transformed)
             reg = flags.alpha * F.mse_loss(base_output_transformed, base_output)
             (metrics.loss + reg).backward()
             metrics.reg = reg
         else:
+            metrics.reg = None
             metrics.loss.backward()
 
-
-        metrics.update_metrics(data[0].shape[0])
+        metrics.update_metrics(x[0].shape[0])
         optim.step()
 
-    print(f'>>> Training: Loss {metrics.loss_meter}, Reg {metrics.regularization_meter}, Acc {metrics.accuracy_meter}, precision: {metrics.precision_meter}, recall{metrics.recall_meter}')
+    print(
+        f'\t>>> Training: Loss {metrics.loss_meter}, Reg {metrics.regularization_meter}, Acc {metrics.accuracy_meter}, precision: {metrics.precision_meter}, recall{metrics.recall_meter}')
 
-    val_metrics = Metrics()
-
-    #model.eval()
+    # model.eval()
     with torch.no_grad():
         for x, y, transformed_image in tqdm(val_loader, ncols=75, leave=False):
             x, y, transformed_image = x.to(device), y.to(device), transformed_image.to(device)
@@ -159,14 +164,16 @@ for epoch in range(flags.epochs):
             val_metrics.update_metrics(x.shape[0])
 
     if flags.use_reg_loss:
-        print(f'>>> Val: Loss {val_metrics.loss_meter}, Reg {val_metrics.regularization_meter}, Acc {metrics.accuracy_meter}, precision: {val_metrics.precision_meter}, recall{metrics.recall_meter}')
+        print(
+            f'\t>>> Val.    : Loss {val_metrics.loss_meter}, Reg {val_metrics.regularization_meter}, Acc {val_metrics.accuracy_meter}, precision: {val_metrics.precision_meter}, recall{val_metrics.recall_meter}')
     else:
-        print(f'>>> Val: Loss {val_metrics.loss_meter}, Acc {metrics.accuracy_meter}, precision: {val_metrics.precision_meter}, recall{metrics.recall_meter}')
+        print(
+            f'\t>>> Val.    : Loss {val_metrics.loss_meter}, Acc {val_metrics.accuracy_meter}, precision: {val_metrics.precision_meter}, recall{val_metrics.recall_meter}')
     # Compute the confusion matrix
-    #cm = confusion_matrix(y_true, y_pred)
+    # cm = confusion_matrix(y_true, y_pred)
 
     # Display the confusion matrix
-    #print(cm)
+    # print(cm)
 
     if val_metrics.loss_meter.float() < best_val_loss:
         best_val_loss = val_metrics.loss_meter.float()
@@ -177,7 +184,7 @@ for epoch in range(flags.epochs):
         best_val_precision = val_metrics.precision_meter.float()
     if val_metrics.accuracy_meter.float() > best_val_acc:
         best_val_acc = val_metrics.accuracy_meter.float()
-        save_path = os.path.join(flags.model_save_dir, 'epoch{}_acc_{:.3f}.ckpt'.format(epoch, best_val_acc))
+        save_path = os.path.join(flags.model_save_dir, 'epoch{}_acc_{:.4f}.ckpt'.format(epoch, best_val_acc))
         torch.save(model.state_dict(), save_path)
         print('Saved model with highest acc ...')
 
